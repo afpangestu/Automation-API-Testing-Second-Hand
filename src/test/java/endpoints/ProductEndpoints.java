4bcbda8af566524ad1b00c4050d10697a67f6f62
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-22 18:19:09 +0700

    add getproduct

--- src/test/java/endpoints/ProductEndpoints.java
+++ src/test/java/endpoints/ProductEndpoints.java
@@ -3,6 +3,7 @@ package endpoints;
 import auth.Authentication;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,7 +12,7 @@ import java.io.File;
 import java.util.ResourceBundle;
 
 public class ProductEndpoints {
-    Authentication authen;
+    Authentication auth;
 
     // get value from properties file
     static ResourceBundle getValue() {
@@ -20,17 +21,17 @@ public class ProductEndpoints {
 
     @BeforeClass
     public void setup() {
-        authen = new Authentication();
+        auth = new Authentication();
     }
 
-    @Test
+    @Test(priority = 1)
     public void postProduct() {
         File img1 = new File("src/test/resources/image/klepon1.png");
         File img2 = new File("src/test/resources/image/garuda biru.jpg");
 
         String url = getValue().getString("postProductUrl");
         Response response = RestAssured.given()
-                .filter(authen.getSession())
+                .filter(auth.getSession())
                 .multiPart("product[name]", "Product xyz array")
                 .multiPart("product[price]", "20000")
                 .multiPart("product[description]", "Ini deskripsi produk")
@@ -42,5 +43,15 @@ public class ProductEndpoints {
                 .post(url);
         response.then().log().body();
         Assert.assertEquals(response.getStatusCode(), 201);
+    }
+
+    @Test(priority = 2)
+    public void getProduct() {
+        String url = getValue().getString("getProductUrl");
+        Response response = RestAssured.given()
+                .queryParam("page",1)
+                .get(url);
+        response.then().log().body();
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 }
