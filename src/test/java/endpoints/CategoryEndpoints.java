f0de6358fd73ad17eb7b047bc0362d93b4e018f5
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-22 21:30:08 +0700

    add testing for Category endpoints

---
+++ src/test/java/endpoints/CategoryEndpoints.java
@@ -0,0 +1,44 @@
+package endpoints;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CategoryEndpoints {
+
+    // get value from properties file
+    static ResourceBundle getValue() {
+        return ResourceBundle.getBundle("routes");
+    }
+
+    @Test
+    public void getCategory() {
+        String url = getValue().getString("getCategoryUrl");
+        Response response = RestAssured.given()
+                .when()
+                .get(url);
+        response.then().log().body();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test
+    public void getCategoryById() {
+        String url = getValue().getString("getCategoryByIdUrl");
+        Response response = RestAssured.given()
+                .pathParam("id", getRandomValue(1, 5))
+                .when()
+                .get(url);
+        response.then().log().body();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    public static int getRandomValue(int min, int max) {
+        // Get and return the random integer
+        // within Min and Max
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+}
---
+++ src/test/java/pojo/Category.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Category{
+	private Category category;
+
+	public void setCategory(Category category){
+		this.category = category;
+	}
+
+	public Category getCategory(){
+		return category;
+	}
+}
